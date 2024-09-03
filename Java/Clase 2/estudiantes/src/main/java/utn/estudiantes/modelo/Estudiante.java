c86295aa49f7d01759187329c4a55c10ec3327b1
David Abadie <abadiedaviiid@gmail.com>
2024-09-02 23:51:49 -0300

    Clase  3 java

---
+++ Java/Clase 2/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,25 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//boilerplate - Repetitivo
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
