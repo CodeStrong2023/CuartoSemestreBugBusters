e2626f2595d78ec05dab81f4eca8a03ab4d03ce4
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-09-11 17:58:18 -0300

    Clase 4 del ecommerce

--- JavaScript/E-COMMERCE2024/client/js/products.js
+++ JavaScript/E-COMMERCE2024/client/js/products.js
@@ -2,29 +2,36 @@ const productos = [
     {
         id: 1,
         productName: "Banana",
-        price: 480,
+        price: 420,
         quanty: 1,
-        img: "/client/media/banana.png"
+        img: "/E-COMMERCE2024/client/media/banana.png"
     },
     {
         id: 2,
         productName: "Leche",
         price: 950,
         quanty: 1,
-        img: "/client/media/leche-fresca.png"
+        img: "/E-COMMERCE2024/client/media/leche-fresca.png"
     },
     {
         id: 3,
         productName: "Pollo",
         price: 780,
         quanty: 1,
-        img: "/client/media/pollo-crudo-fresco.png"
+        img: "/E-COMMERCE2024/client/media/pollo-crudo-fresco.png"
     },
     {
         id: 4,
         productName: "Mayonesa",
         price: 510,
         quanty: 1,
-        img: "/client/media/mayonesa.png"
+        img: "/E-COMMERCE2024/client/media/mayonesa.png"
     },
+    {
+        id: 5,
+        productName: "Enlatados",
+        price: 520,
+        quanty: 1,
+        img: "/E-COMMERCE2024/client/media/enlatados.png"
+    }
 ]
