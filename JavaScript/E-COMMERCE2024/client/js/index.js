1d581a98578bfdd5dae3eb6de4ad5c26261daedd
FranquiPI <pizarrofranco25@gmail.com>
2024-09-11 10:56:41 -0300

    Clase 3 E-commerce basico con Javascript (Laboratorio IV)

--- JavaScript/E-COMMERCE2024/client/js/index.js
+++ JavaScript/E-COMMERCE2024/client/js/index.js
@@ -17,13 +17,23 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        });
-        console.log(cart);
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((product)=> {
+                if(prod.id === product.id){
+                    prod.quanty++;
+                }
+            });
+        }else{
+             cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
     });
 });
+       
