956e418ae1314223583e25d26c24b7ad951d6bae
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-09-18 19:26:23 -0300

    ultimas modificaciones al proyecto

--- Java/Clase2,3,4y5/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante2024.java
+++ Java/Clase2,3,4y5/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante2024.java
@@ -1,15 +1,11 @@
 package utn.estudiantes.modelo;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import jakarta.persistence.Table;
 
 @Entity
 //boilerplate - Repetitivo
@@ -21,6 +17,7 @@ import jakarta.persistence.Table;
 public class Estudiante2024 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estudiante")
     private Integer idestudiantes2024;
     private String nombre;
     private String apellido;
