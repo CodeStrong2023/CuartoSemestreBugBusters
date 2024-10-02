49d4b6454f8885d2d60ba614492dc40584b855ea
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-10-01 21:19:24 -0300

    Segunda parte del proyecto de la tienda de libros

---
+++ Java/Clase6/tienda_libros/src/main/java/utn/tienda_libros/modelo/Libro.java
@@ -0,0 +1,24 @@
+package utn.tienda_libros.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idLibro;
+    String nombreLibro;
+    String autor;
+    Double precio;
+    Integer existencias;
+
+}
